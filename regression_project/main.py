63396bf79c0957fa65b5ff6bd33df889bf3cfa79
raviteja869 <132315611+raviteja869@users.noreply.github.com>
2023-09-30 13:29:52 -0400

    Update main.py

--- regression_project/main.py
+++ regression_project/main.py
@@ -1,3 +1,4 @@
+import os
 from src.data_preparation import generate_regression_data
 from src.model import create_regression_model
 from src.train import train_regression_model
@@ -14,3 +15,6 @@ train_regression_model(regression_model, X_train, y_train, epochs=100, batch_siz
 sample_input = [[0.5]]  # Example input for prediction
 predicted_value = predict_regression(regression_model, sample_input)
 print(f"Predicted value: {predicted_value[0][0]}")
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
