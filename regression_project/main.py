3ddcd928216b4bfe6544f0b8f0e899c65368fb50
raviteja869 <132315611+raviteja869@users.noreply.github.com>
2023-09-30 19:08:18 -0400

    Update main.py

--- regression_project/main.py
+++ regression_project/main.py
@@ -1,8 +1,18 @@
 import os
+from flask import Flask, jsonify
 from src.data_preparation import generate_regression_data
 from src.model import create_regression_model
 from src.train import train_regression_model
 from src.predict import predict_regression
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    # Example input for prediction
+    sample_input = [[0.5]]
+    predicted_value = predict_regression(regression_model, sample_input)
+    return jsonify({"Predicted value": predicted_value[0][0]})
 
 # Generate synthetic regression data
 X_train, y_train = generate_regression_data()
@@ -11,14 +21,6 @@ X_train, y_train = generate_regression_data()
 regression_model = create_regression_model(X_train.shape[1:])
 train_regression_model(regression_model, X_train, y_train, epochs=100, batch_size=32)
 
-# Perform regression prediction
-sample_input = [[0.5]]  # Example input for prediction
-predicted_value = predict_regression(regression_model, sample_input)
-print(f"Predicted value: {predicted_value[0][0]}")
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
-
-from flask import Flask
-app = Flask(__name__)
-
