b1c5c488237d5e9e533ee7e84d22223ddfa553e1
raviteja869 <132315611+raviteja869@users.noreply.github.com>
2023-09-30 13:47:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        # List your dependencies here, e.g.,
+        # List your dependencies here
         "tensorflow==2.4.0",
         "numpy==1.19.0",
         "pytest==6.2.0",
-        "pytest-cov==2.10.0"
+        "pytest-cov==2.10.0",
+        "botocore>=1.20.0",
+        "awscli>=1.19.0",
+        "urllib3>=1.26.0",
+        "rsa>=4.7.0",
+        "Flask>=1.1.2"
     ],
 )
-
